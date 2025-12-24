3bf7fc79d12bdb8a6c53b402e2502f22acffb357
Huseyn <hsbziyev@gmail.com>
2025-12-24 23:47:16 +0400

    Firstt

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+
+const App = () => {
+    return (
+        <div className='max-w-360 w-full mx-auto'>
+            <Navbar />
+        </div>
+    )
+}
+
+export default App
